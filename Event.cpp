d8c120fc0442518107b37c2625481b4c210ecb8f
Alex Hartstone <alex.hartstone@gmail.com>
2018-10-26 08:56:28 +1100

    deleted mysterious files

--- Event.cpp
+++
@@ -1,3 +0,0 @@
-#include "Event.hpp"
-
-unsigned int Event::count = 0;
--- EventDebug.cpp
+++
@@ -1,9 +0,0 @@
-#include "Event.hpp"
-
-std::string EventDebug::getMsg() {
-	return this->msg;
-}
-
-int EventDebug::getLevel() {
-	return this->level;
-}
